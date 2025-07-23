7f753fb2ed4a128e180918d817752b620d66523c
DarshanK2403 <darshandeesa2403@gmail.com>
2025-07-23 20:59:15 +0530

    update Logo

--- src/app/(main)/loading.js
+++ src/app/(main)/loading.js
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center bg-white text-gray-800 h-28 m-auto h-screen">
+    <div className="flex items-center justify-center bg-white text-gray-800 m-auto h-screen">
       <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>
     </div>
   );
